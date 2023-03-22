33da4592961b83d64f47f39bf47c7c6512af865e
My Name <tadzhibaevaelnura@gmail.com>
2023-03-23 02:37:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(4);
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(3);
+        list2.add(4);
+
+        List<Integer> merge = new ArrayList<>();
+        merge.addAll(list1);
+        merge.addAll(list2);
+        System.out.println(merge);
+    }
+}
